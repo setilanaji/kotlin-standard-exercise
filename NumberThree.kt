9bd02fc0a1e9c3393e0eb75634080b6bb5469583
PC-2 <setyajiyudha299@gmail.com>
2020-11-06 11:39:55 +0700

    update number three: add function to find total buy of user

--- NumberThree.kt
+++ NumberThree.kt
@@ -2,17 +2,33 @@ import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 import java.io.File
 
-
 fun main() {
-//    val jsonString: String = File("./src/main/kotlin/three.json").readText(Charsets.UTF_8)
-//    print(jsonString)
     val result = getResourceToList("./src/main/kotlin/three.json")
-//    println(result.toString())
+    getRecordByName("Ari", result).also { print(getTotalRecord(it, it!!.size)) }
+    getCustomerNameFilterByMaxBuy(300000, result).also { print(it) }
 }
 
-
 fun getResourceToList(path: String): List<Record>? {
-    return Klaxon().parseArray<Record>(File(path))
+    return Klaxon().parseArray(File(path))
+}
+//fun getRecordByMonth(month: Month): List<Record>?{
+//
+//}
+
+fun getRecordByName(name: String, records: List<Record>?): List<Record>?{
+    return records!!.filter { name == it.customer.name}
+}
+
+//Recursive function
+fun getTotalRecord(records: List<Record>?, index: Int): Int{
+    return if (index <= 0) 0 else (getTotalRecord(records, index-1) +
+            (records?.get(index - 1)?.items?.sumBy { item -> item.price * item.qty }
+        ?: 0 ))
+}
+
+//TODO: continue this function
+fun getCustomerNameFilterByMaxBuy(max: Int, records: List<Record>?): List<String>{
+    return records!!.filter {  it.items.sumBy { item -> item.price * item.qty } < max }.distinctBy { it.customer.name }.map { it.customer.name }
 }
 
 data class Record(
